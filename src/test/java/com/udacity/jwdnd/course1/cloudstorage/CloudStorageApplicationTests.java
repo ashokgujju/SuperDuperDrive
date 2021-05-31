b7e883bfa7f73a73b7f132fe0321ee92393023bd
Ashok Kumar Gujju <ashokg.geek@gmail.com>
2021-05-31 22:10:46 +0530

    test user signup, login and unauthorized access restrictions

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -7,35 +7,75 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
 	private int port;
 
-	private WebDriver driver;
+	private static WebDriver driver;
+
+	public String baseURL;
 
 	@BeforeAll
 	static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+
+	@AfterAll
+	static void afterAll() {
+		if (driver != null) {
+			driver.quit();
+		}
 	}
 
 	@BeforeEach
 	public void beforeEach() {
-		this.driver = new ChromeDriver();
-	}
-
-	@AfterEach
-	public void afterEach() {
-		if (this.driver != null) {
-			driver.quit();
-		}
+		baseURL = "http://localhost:" + port;
 	}
 
 	@Test
 	public void getLoginPage() {
 		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
+		assertEquals("Login", driver.getTitle());
 	}
 
+	@Test
+	public void testUnauthorizedUserCanOnlyAccessLoginAndSignupPages() {
+		driver.get(baseURL + "/home");
+		assertNotEquals("Home", driver.getTitle());
+
+		driver.get(baseURL + "/login");
+		assertEquals("Login", driver.getTitle());
+
+		driver.get(baseURL + "/signup");
+		assertEquals("Sign Up", driver.getTitle());
+	}
+
+	@Test
+	public void testHomePageAccessAfterLoginAndAfterLogout() {
+		String username = "starlord";
+		String password = "milano";
+
+		driver.get(baseURL + "/signup");
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup("Peter", "Quill", username, password);
+
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
+
+		driver.get(baseURL + "/home");
+		assertEquals("Home", driver.getTitle());
+
+		driver.get(baseURL + "/logout");
+
+		driver.get(baseURL + "/home");
+		assertNotEquals("Home", driver.getTitle());
+	}
 }
